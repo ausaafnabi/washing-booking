fda4319e3b619cfde61a40c9e5bca4042613ad52
Ausaaf Nabi <nabiausaaf@gmail.com>
2022-11-14 08:54:15 +0000

    Alter Bookings and Slot Schema

--- washing/slot/models.py
+++ washing/slot/models.py
@@ -4,9 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Slot(models.Model):
     machineTime = models.TimeField()
+    to = models.TimeField()
 
 class Booking(models.Model):
     Date = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     slot_id = models.ForeignKey(Slot, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+class perDy(models.Model):
+    count = models.BooleanField(verbose_name="")
 

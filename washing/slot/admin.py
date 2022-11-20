3610a92e97bfcc9b3f67333a99f2a74c27c99837
Ausaaf Nabi <nabiausaaf@gmail.com>
2022-11-20 12:33:50 +0000

    Add Bookings and updated schema

--- washing/slot/admin.py
+++ washing/slot/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from slot.models import Slot,Booking
+from slot.models import Slot,Booking,Machine
 
 class SlotAdmin(admin.ModelAdmin):
     pass
 
 class BookingAdmin(admin.ModelAdmin):
     pass
+class MachineAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Slot, SlotAdmin)
 admin.site.register(Booking, BookingAdmin)
+admin.site.register(Machine, MachineAdmin)
--- washing/slot/models.py
+++ washing/slot/models.py
@@ -3,14 +3,24 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Slot(models.Model):
-    machineTime = models.TimeField()
-    to = models.TimeField()
+    start= models.TimeField()
+    end = models.TimeField()
+
+class Machine(models.Model):
+    macName = models.CharField(max_length=20,unique=True)
+    slots = models.ManyToManyField(to=Slot)
+    address = models.CharField(max_length=80)
+    comments = models.TextField()
+    status = models.CharField(max_length=20)
 
 class Booking(models.Model):
     Date = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     slot_id = models.ForeignKey(Slot, on_delete=models.CASCADE)
     comment = models.TextField()
+
+class Status(models.Model):
+    stat=models.CharField(max_length=20)
 
 class perDy(models.Model):
     count = models.BooleanField(verbose_name="")

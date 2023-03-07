a7ab6a9219c85deb65a6f01c2fff233b61c0513d
Ausaaf Nabi <nabiausaaf@gmail.com>
2023-03-07 22:18:26 +0000

    Updated schema for multiple machines

--- washing/slot/models.py
+++ washing/slot/models.py
@@ -5,17 +5,27 @@ from django.contrib.auth.models import User
 class Slot(models.Model):
     start= models.TimeField()
     end = models.TimeField()
-
+    
 class Machine(models.Model):
     macName = models.CharField(max_length=20,unique=True)
     slots = models.ManyToManyField(to=Slot)
-    address = models.CharField(max_length=80)
+    #address = models.CharField(max_length=80)
+    roomNo = models.CharField(max_length=3)
+    dormNo = models.CharField(max_length=2)
     comments = models.TextField()
     status = models.CharField(max_length=20)
 
 class Booking(models.Model):
     Date = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    machine_id = models.ForeignKey(Machine,on_delete=models.CASCADE)
+    slot_id = models.ForeignKey(Slot, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+class Booking(models.Model):
+    Date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    machine_id = models.ForeignKey(Machine,on_delete=models.CASCADE)
     slot_id = models.ForeignKey(Slot, on_delete=models.CASCADE)
     comment = models.TextField()
 
